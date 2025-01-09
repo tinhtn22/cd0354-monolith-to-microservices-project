af1b63b5f985a6ec0329b0072f3e6fd189e02ea5
Tinh Tran Ngoc <tinhtn@fpt.com>
2025-01-09 13:56:26 +0900

    refactor app

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,14 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=tinhtn298
+export POSTGRES_HOST=database-1.crfpf4gs88di.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=project3bucket-tinhtn
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRpbmh0bjJAZ21haWwuY29tIiwiaWF0IjoxNzM2MzgzMDI4fQ.VjbTXWwsnzRDTU7bniP3NLaL-RZjEl0paRa2LaWu6ys
 export URL=http://localhost:8100
+# for ionic build
+export NODE_OPTIONS=--openssl-legacy-provider
+export HOME=C:/Users/TinhTN
